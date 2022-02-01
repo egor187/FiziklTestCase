93699a743349d397a65a78d5390eb624b5bffaf7
egor187 <py.egor.py@gmail.com>
2022-02-01 16:35:01 +0300

    plug urls

--- WeekDayResolver/urls.py
+++ WeekDayResolver/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from WeekDayResolver.views import MainView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('weekday-resolver/', MainView.as_view()),
 ]
